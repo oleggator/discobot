2d99951e10e0fb0036c25552e508ad25b8ecc799
Oleg Utkin <o.utkin@corp.mail.ru>
2022-12-17 23:00:43 +0300

    style fix, add progress bar for playing

--- main.go
+++ main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
-	"discobot/webm"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
-	"github.com/kkdai/youtube/v2"
-	"github.com/schollz/progressbar/v3"
 	"io"
 	"log"
 	"net/http"
@@ -16,6 +12,12 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/kkdai/youtube/v2"
+	"github.com/schollz/progressbar/v3"
+
+	"discobot/webm"
 )
 
 const nggyu = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
@@ -47,7 +49,7 @@ func main() {
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
+	// This isn't required in this specific example, but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
@@ -128,15 +130,16 @@ func playSound(s *discordgo.Session, guildID, channelID string) error {
 		return err
 	}
 
+	bar = progressbar.NewOptions(-1, progressbar.OptionShowBytes(true))
+	defer bar.Close()
 loop:
 	for {
-		t := time.NewTimer(5 * time.Second)
+		timeout := time.NewTimer(5 * time.Second)
 		select {
 		case packet := <-r.Chan:
 			vc.OpusSend <- packet.Data
-			fmt.Print(".")
-		case <-t.C:
-			log.Println("stopped")
+			_ = bar.Add(len(packet.Data))
+		case <-timeout.C:
 			break loop
 		}
 	}
